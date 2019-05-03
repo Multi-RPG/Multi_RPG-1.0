0f5360f4bef491a6129cfafde2c881ac6dc5445a
jdkennedy45 <jdkennedy45@students.tntech.edu>
2019-05-03 17:37:27 -0400

    added profile page function for pet details

--- Pets.py
+++ Pets.py
@@ -102,11 +102,50 @@ class Pets:
             else:
                 pet_avatar = "https://cdn.discordapp.com/emojis/422845006232027147.png?v=1"
 
-            confirmation_msg = "Fed **" + pet_name + "**! They feel stuffed now." \
+            confirmation_msg = "Fed **" + pet_name + "**! They feel stuffed for today." \
                                                      "\n\n\n**XP:** " + str(new_pet_xp) + "/" + str(level_up_cost)
 
         # embed confirmation message, then overwrite the variable with the actual message object
         em = discord.Embed(description=confirmation_msg, colour=0x607d4a)
+        em.set_thumbnail(url=pet_avatar)
+        await self.client.say(embed=em)
+
+    '''FEED PET FUNCTION'''
+
+    @has_account()
+    @commands.cooldown(1, 1, commands.BucketType.user)
+    @commands.command(name='pet', description='check your pet status', brief='can use =pet',
+                      aliases=['PET', 'Pet'], pass_context=True)
+    async def pet_profile(self, context):
+        # create instance of the user who wishes to feed their pet
+        pet_owner = Users(context.message.author.id)
+        # retrieve pet name
+        pet_name = pet_owner.get_user_pet_name()
+        # retrieve integer of pet's current xp
+        pet_xp = pet_owner.get_user_pet_xp(0)
+        # retrieve integer of pet's current level
+        pet_level = pet_owner.get_user_pet_level(0)
+
+        # calculate the current level up requirement for the user's pet
+        level_up_cost = int(300 * ((pet_level + 1) ** 1.20) - (300 * pet_level))
+
+        if pet_level == 1:
+            pet_avatar = "https://cdn.discordapp.com/emojis/563872560308289536.png?v=1"
+        elif pet_level == 2:
+            pet_avatar = "https://cdn.discordapp.com/emojis/491930617823494147.png?v=1"
+        elif pet_level == 3:
+            pet_avatar = "https://cdn.discordapp.com/emojis/400690504104148992.png?v=1"
+        elif pet_level == 4:
+            pet_avatar = "https://cdn.discordapp.com/emojis/400681095009533962.png?v=1"
+        else:
+            pet_avatar = "https://cdn.discordapp.com/emojis/422845006232027147.png?v=1"
+
+        pet_details = "**" + pet_name + "** (Pet Profile) " \
+                                                     "\n\n\n**Level: **" + str(pet_level) + \
+                                                     "\n**XP:** " + str(pet_xp) + "/" + str(level_up_cost)
+
+        # embed confirmation message, then overwrite the variable with the actual message object
+        em = discord.Embed(description=pet_details, colour=0x607d4a)
         em.set_thumbnail(url=pet_avatar)
         await self.client.say(embed=em)
 
