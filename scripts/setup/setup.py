9f3098d0afdc4f674adeada7ae9c9bf22a9b8da9
jdkennedy45 <jdkennedy45@students.tntech.edu>
2019-05-15 03:59:24 -0400

    simplified setup script

--- scripts/setup/setup.py
+++ scripts/setup/setup.py
@@ -11,28 +11,14 @@ import zipfile
 import sys
 
 
-def copy_directories():
+def extract_directories():
     # extract zip with setup files
-    zip = zipfile.ZipFile('setup_files.zip', 'r')
-    zip.extractall('../..')
-    zip.close()
-
-    # create database from schema extracted
-    create_database()
+    zipper = zipfile.ZipFile('setup_files.zip', 'r')
+    zipper.extractall('../..')
+    zipper.close()
 
 
 def create_database():
-    # check if database file already exists
-    if os.path.isfile('../../db_and_words/hangman.db'):
-        # confirm if user wants to overwrite the database
-        confirm = input("Database file already exists. Are you sure you want to override it? Type yes or no:\n")
-        # overwrite the database if they do, and continue on
-        if confirm.lower() == 'yes':
-            os.remove('../../db_and_words/hangman.db')
-        # exit script if they do not
-        else:
-            sys.exit(0)
-
     # create database
     connection = sqlite3.connect('../../db_and_words/hangman.db')
     # open our database schema
@@ -42,5 +28,20 @@ def create_database():
 
 
 if __name__ == "__main__":
-    copy_directories()
+    # check if database file already exists
+    if os.path.isfile('../../db_and_words/hangman.db'):
+        # confirm if user wants to overwrite the database
+        confirm = input("Database file already exists. Are you sure you want to continue with setup? Type yes or no:\n")
+        # delete the current database if they do, and continue on
+        if confirm.lower() == 'yes':
+            os.remove('../../db_and_words/hangman.db')
+        # exit script if they do not
+        else:
+            print("Setup cancelled!")
+            sys.exit(0)
+
+    # extract directories with setup files
+    extract_directories()
+    # create database from schema extracted
+    create_database()
     print("Setup complete! Please continue to edit tokenbot.ini in the tokens folder.")
